f1ce5aa42c1f7e7e675d3800e1b0b02010bdee7e
nilanjan-joarder <joardernilanjan@gmail.com>
2026-05-27 23:11:21 +0530

    Learned arrays, objects, map() and rendering list from data

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+
 export default function HomeScreen() {
-  const [likes, setLikes] = useState(0);
-  function getLikeMessage() {
-    return likes > 5 ? (
-      <Text style={{ color: 'white', fontSize: 20 }}>Popular</Text>
-    ) : (
-      <Text style={{ color: 'white', fontSize: 20 }}>Not Popular</Text>
-    );
-  }
+  const users = [
+    {
+      name: "Rahul",
+      role: "Engineer"
+    },
+    {
+      name: "Priya",
+      role: "Designer"
+    },
+    {
+      name: "Arjun",
+      role: "Product Manager"
+    }
+  ];
 
   return (
     <View
@@ -17,28 +23,17 @@ export default function HomeScreen() {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Button
-        title="Like"
-        onPress={() => {
-          setLikes(likes + 1);
-        }}
-      />
-      <Button
-        title="Reset"
-        onPress={() => {
-          setLikes(0);
-        }}
-      />
-      <Button
-        title="DisLike"
-        onPress={() => {
-          if (likes > 0) {
-            setLikes(likes - 1);
-          }
-        }}
-      />
-      <Text style={{ color: 'white', fontSize: 20 }}>Likes: {likes}</Text>
-      {getLikeMessage()}
+      {users.map((user, index) => (
+        <View key={index}>
+          <Text style={{ color: 'white' }}>
+            {user.name}
+          </Text>
+
+          <Text style={{ color: 'white' }}>
+            {user.role}
+          </Text>
+        </View>
+      ))}
     </View>
   );
 }
