85d199e3199f4a91ba16907a53b38b19ceedca90
nilanjan-joarder <joardernilanjan@gmail.com>
2026-05-29 22:43:16 +0530

    Learn props TypeScript types destructuring and StyleSheet

--- src/components/UserCards.tsx
+++ src/components/UserCards.tsx
@@ -1,19 +1,27 @@
-import { Text, View } from 'react-native';
-
-export default function UserCard(props: any) {
-  return (
-    <View
-  style={{
+import { StyleSheet, Text, View } from 'react-native';
+type UserCardProps = {
+  name: string;
+  role: string;
+};
+const styles = StyleSheet.create({
+  card: {
     alignItems: 'center',
     marginBottom: 20,
-  }}>
-  <Text style={{ color: 'white' }}>
-    {props.name}
-  </Text>
-
-  <Text style={{ color: 'white' }}>
-    {props.role}
-  </Text>
-</View>
+  },
+  name: {
+    color: 'white',
+    fontSize: 14,
+  },
+  role: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+export default function UserCard({name,role,}: UserCardProps) {
+  return (
+    <View style={styles.card }>
+      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.role}>{role}</Text>
+    </View>
   );
 }
