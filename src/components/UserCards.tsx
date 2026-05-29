4ad047c527586b85a419cb5479f068d4ca4f79d1
nilanjan-joarder <joardernilanjan@gmail.com>
2026-05-29 23:01:42 +0530

    Learn Pressable component state and toggle behavior

--- src/components/UserCards.tsx
+++ src/components/UserCards.tsx
@@ -1,13 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text } from 'react-native';
+import { useState } from 'react';
 type UserCardProps = {
   name: string;
   role: string;
 };
 const styles = StyleSheet.create({
   card: {
-    alignItems: 'center',
-    marginBottom: 20,
-  },
+  alignItems: 'center',
+  marginBottom: 20,
+  padding: 20,
+  borderWidth: 1,
+  borderColor: 'white',
+  borderRadius: 12,
+},
   name: {
     color: 'white',
     fontSize: 14,
@@ -17,11 +22,21 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
 });
-export default function UserCard({name,role,}: UserCardProps) {
+export default function UserCard({ name, role, }: UserCardProps) {
+  const [clicks, setClicks] = useState(0);
+  const [selected, setSelected] = useState(false);
   return (
-    <View style={styles.card }>
+    <Pressable style={styles.card} onPress={() => {
+      setClicks(clicks + 1);
+      setSelected(!selected);
+    }}>
       <Text style={styles.name}>{name}</Text>
       <Text style={styles.role}>{role}</Text>
-    </View>
+      <Text style={styles.role}>
+        Clicks: {clicks} </Text>
+      <Text style={styles.role}>
+        {selected ? 'Selected' : 'Not Selected'}
+      </Text>
+    </Pressable >
   );
 }
